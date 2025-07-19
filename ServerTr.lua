207ba0e4a6733988d6452923fde216adc0d8c078
Goliate <75161053+goliate200@users.noreply.github.com>
2025-07-19 20:40:27 +0100

    Update ServerTr.lua

--- ServerTr.lua
+++ ServerTr.lua
@@ -6,7 +6,7 @@ spawn(function()
         Webhook = "https://discord.com/api/webhooks/1368315112737210398/ydQykT7Wt2IqZy94QkmVXOHkgE5jvPw0mYNSiUNJBn2ClRlk0-M7GFLcD6IjwsnWwLGU",
         Ping = "Secret Hatched <@&785853304328421447>",
         Rarities = {"Legendary", "Secret", "Infinity"},
-        Ignore = {"Unicorn", "Paragon", "Emerald Golem", "Platinum Dualcorn", "Void Eye", "Neon Wyvern", "Soarin' Surfer", "Neon Elemental", "Lunar Serpent", "Electra", "Inferno Dragon", "Cyborg Phoenix", "Crystal Unicorn", "Green Hydra", "Inferno Cube", "Dark Phoenix", "Flying Pig", "Fossilus", "Kiwi Dragon", "Popsicle Trio"},
+        Ignore = {"Unicorn", "Angel Bee", "Paragon", "Emerald Golem", "Platinum Dualcorn", "Void Eye", "Neon Wyvern", "Soarin' Surfer", "Neon Elemental", "Lunar Serpent", "Electra", "Inferno Dragon", "Cyborg Phoenix", "Crystal Unicorn", "Green Hydra", "Inferno Cube", "Dark Phoenix", "Flying Pig", "Fossilus", "Kiwi Dragon", "Popsicle Trio"},
         SkipEasyLegends = true,
         HatchAmounts = false,
         Server = true,
